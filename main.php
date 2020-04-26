82e5dcac3625b16f36d3754d8aebe85cd698250e
Yui-Ezic <mishan221199@gmail.com>
2020-04-26 19:15:09 +0300

    Json output to file

--- main.php
+++ main.php
@@ -11,11 +11,12 @@ use Antlr\Antlr4\Runtime\InputStream;
 use Antlr\Antlr4\Runtime\Utils\StringUtils;
 use Generated\Z99Lexer;
 use Generated\Z99Parser;
+use function sprintf;
 
 function token_to_string(CommonToken $token, array $tokenMap)
 {
     $tokenType = array_key_exists($token->getType(), $tokenMap) ? $tokenMap[$token->getType()] : 'EOF';
-    return \sprintf(
+    return sprintf(
         '@%02d:%02d  %-10s %s \'%s\'',
         $token->getLine(),
         $token->getCharPositionInLine(),
@@ -25,6 +26,17 @@ function token_to_string(CommonToken $token, array $tokenMap)
     ) . PHP_EOL;
 }
 
+function token_to_array(CommonToken $token, array $tokenMap)
+{
+    $tokenType = array_key_exists($token->getType(), $tokenMap) ? $tokenMap[$token->getType()] : 'EOF';
+    return [
+        'line' => $token->getLine(),
+        'type' => $tokenType,
+        'string' => StringUtils::escapeWhitespace($token->getText() ?? ''),
+        'index' => null
+    ];
+}
+
 $input = InputStream::fromPath('example.z99');
 $lexer = new Z99Lexer($input);
 $lexer->addErrorListener(new ConsoleErrorListener());
@@ -32,10 +44,14 @@ $tokens = new CommonTokenStream($lexer);
 $tokens->fill();
 $tokenMap = $lexer->getTokenTypeMap();
 $tokenMap = array_flip($tokenMap);
+$file = fopen('output.txt', 'wb');
+$array = [];
 foreach ($tokens->getAllTokens() as $token) {
     /* @var $token CommonToken */
     echo token_to_string($token, $tokenMap);
+    $array[] = token_to_array($token, $tokenMap);
 }
+fwrite($file, json_encode($array));
 $parser = new Z99Parser($tokens);
 $parser->addErrorListener(new ConsoleErrorListener());
 $parser->setBuildParseTree(true);
